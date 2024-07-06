d191b97130f1c9b22cfb773bdb15b6f331a78514
AkeelAslam <akeelcourseaslam@gmail.com>
2024-07-06 20:30:46 +0530

    modified

---
+++ AkeelApp/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using AkeelApp.Data;
+using AkeelApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AkeelApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db) {
+            _db=db;
+        
+        }
+        public IActionResult Index()
+        {
+            List<Category> objectCategoryList=_db.categories.ToList();
+            return View();
+        }
+    }
+}
---
+++ AkeelApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using AkeelApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AkeelApp.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options) 
+        {
+            
+        }
+        public DbSet<Category> categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Akeel", DisplayOrder = 1 },
+                  new Category { Id = 2, Name = "Akeel", DisplayOrder = 2 },
+                    new Category { Id = 3, Name = "Akeel", DisplayOrder = 3 });
+        }
+    }
+}
